403345f07c1ad84d0fcbf28f9fe0386aa2c1f779
ThuatVDQ <thuatvopro@gmail.com>
2024-06-06 16:57:21 +0700

    add: init project

---
+++ src/test/java/com/mycompany/employeemanagement/EmployeeManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mycompany.employeemanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmployeeManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
